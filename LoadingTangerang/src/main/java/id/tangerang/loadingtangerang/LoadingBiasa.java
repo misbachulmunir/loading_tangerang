eefe72e56e92ae40dbceabefdd35e5477f61dba8
Misbachul Munir <misbachulmunir2905@gmail.com>
2022-03-11 10:40:32 +0700

    perbaikan

--- LoadingTangerang/src/main/java/id/tangerang/loadingtangerang/LoadingBiasa.java
+++ LoadingTangerang/src/main/java/id/tangerang/loadingtangerang/LoadingBiasa.java
@@ -2,46 +2,29 @@ package id.tangerang.loadingtangerang;
 
 import android.app.ProgressDialog;
 import android.content.Context;
-import android.widget.Toast;
-
 public class LoadingBiasa {
     private final Context context;
     private ProgressDialog progressDialog;
+    private Boolean cancelable;
+    private Boolean cancelableoutdialog;
 
-    public LoadingBiasa(Context context) {
+    public LoadingBiasa(Context context,Boolean cancelable,Boolean cancelableoutdialog) {
         this.context = context;
         progressDialog = new ProgressDialog(context);
+        this.cancelable = cancelable;
+        this.cancelableoutdialog = cancelableoutdialog;
     }
-
-
     public void showDialog() {
-        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.setCanceledOnTouchOutside(cancelableoutdialog);
         progressDialog.show();
-        progressDialog.setCancelable(false);
+        progressDialog.setCancelable(cancelable);
         progressDialog.setContentView(R.layout.loading);
         progressDialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
 
     }
-
-
-
     public void dismissDialog() {
         if (progressDialog.isShowing()) {
             progressDialog.dismiss();
-        }
-    }
-
-    public void toastError() {
-        Toast.makeText(context, "Terjadi kesalahan.", Toast.LENGTH_SHORT).show();
-    }
-
-
-    public double parseDouble(String input) {
-        try {
-            return Double.parseDouble(input);
-        } catch (Exception e) {
-            Toast.makeText(context, "Terjadi kesalahan", Toast.LENGTH_SHORT).show();
-            return 0;
         }
     }
 }
