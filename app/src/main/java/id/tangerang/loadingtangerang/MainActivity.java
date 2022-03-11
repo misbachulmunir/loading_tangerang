3b2a9e6abe8640354a9d7436393f6b12eafe65a4
Misbachul Munir <misbachulmunir2905@gmail.com>
2022-03-11 14:19:33 +0700

    done v.1

--- app/src/main/java/id/tangerang/loadingtangerang/MainActivity.java
+++ app/src/main/java/id/tangerang/loadingtangerang/MainActivity.java
@@ -14,11 +14,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         buttonView = findViewById(R.id.buttonView);
-        LoadingCustom loadingBiasa = new LoadingCustom(context,true,true,R.drawable.chibi);
+        LoadingCustom loading = new LoadingCustom(context,true,true,R.drawable.chibi2);
         buttonView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                loadingBiasa.showDialog();
+                loading.showDialog();
             }
         });
     }
